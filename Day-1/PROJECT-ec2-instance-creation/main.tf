8543e4eb56ac340f34fb0e2c77707cdb44e57fba
Ville <villers118@yahoo.com>
2025-07-28 18:54:04 +0100

    Update main.tf
    ami-0d0ad8bb301edb745

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,9 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "ap-south-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0d0ad8bb301edb745"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+
 }
